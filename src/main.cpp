9d6844c04d536d6a506667496dcc6b4c95dac827
AaryaTalgaonkar <aaryatalgaonkar@gmail.com>
2026-03-07 18:07:07 +0530

    icon for linux

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QApplication::setApplicationName("IITDelhiVPN");
+    QApplication::setDesktopFileName("iitdelhivpn");
     QFontDatabase::addApplicationFont(":/fonts/TitilliumWeb-Regular.ttf");
     QApplication::setFont(QFont("Titillium Web"));
     a.setWindowIcon(QIcon(":/img/logo.png"));
